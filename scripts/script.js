791806eeecbd585531e4fdb3f2565ee0928f4366
bhawna-gupta-it <88166624+bhawna-gupta-it@users.noreply.github.com>
2021-12-10 15:59:26 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,38 @@
-//pass fahrenheit value as input to the function
-function convert(valNum) {
-    valNum = parseFloat(valNum);
-    var celciusResult = (valNum-32)/1.8;
-	//alert(celciusResult)    
-    var outputField = document.getElementById("outputCelcius");
-    outputField.innerHTML = celciusResult +  " celcius";
-  }
-  
-//get fahrenheit from the dom
-function convert1() {	
-	var valNum = document.getElementById("inputTemp").value;
-    valNum = parseFloat(valNum);
-	var outputField = document.getElementById("outputCelcius");
-    var celciusResult = (valNum-32)/1.8;    
-    outputField.innerHTML = "" + celciusResult +  " degree celcius";
+function convert1(inputString ) {
+	//convert input string to float
+	inputNumber = parseFloat(inputString);
+	//apply the formula
+	resultInCelcius = (inputNumber - 32)/1.8;
+	//grab output span
+	outputSpan = document.getElementById("outputCelcius");
+	//display in result
+	outputSpan.innerHTML = resultInCelcius ;
+	//alert(resultInCelcius);
+	//return resultInCelcius;
+}
+
+
+function convert2() {	
+    	var valNum = document.getElementById("inputTemp").value;
+    	valNum = parseFloat(valNum);
+    	var outputField = document.getElementById("outputCelcius");
+    	var celciusResult = (valNum-32)/1.8;    
+    	var suffix = "degree celcius";
+    	outputField.innerHTML = `${celciusResult} ${suffix} `;
   }
   
 //validate the input
-function convert2() {	
-	var valNum = document.getElementById("inputTemp").value;
-    valNum = parseFloat(valNum);
-	if(isNaN(valNum))
-	{
+function convert3() {	
+    	var outputField = document.getElementById("outputCelcius");
+    	var valNum = document.getElementById("inputTemp").value;
+    	valNum = parseFloat(valNum);
+    	if(isNaN(valNum)){
 		document.getElementById("errorText").innerHTML = "Invalid input";
-	}
-	else
-	{
+		outputField.innerHTML = "";
+    	}
+    	else {
 		document.getElementById("errorText").innerHTML = "";
-	}
-	var outputField = document.getElementById("outputCelcius");
-    var celciusResult = (valNum-32)/1.8;    
-   
-	outputField.innerHTML = "" + celciusResult +  " degree celcius";
-  }
+		var celciusResult = (valNum-32)/1.8;    
+   		outputField.innerHTML = "" + celciusResult +  " degree celcius";
+  	}
+}
